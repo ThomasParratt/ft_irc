5f480cb566458639b914263e6301defc5161f317
root <root@LAPTOP-J70A2AVE>
2024-11-22 11:01:52 +0200

    sending and receiving

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include <string.h>
 
+// write in a more c++ way
+// add classes
+// add poll
+// add error management
+
 int main(int argc, char **argv)
 {
     if (argc == 3)
@@ -16,11 +21,7 @@ int main(int argc, char **argv)
 
         // Set the SO_REUSEADDR option
         int opt = 1;
-        if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) 
-        {
-            std::cerr << "Error setting SO_REUSEADDR" << std::endl;
-            return 1;
-        }
+        setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
         // Define the server address
         sockaddr_in serverAddress;
@@ -28,10 +29,10 @@ int main(int argc, char **argv)
         serverAddress.sin_port = htons(port);
         serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-        // Bind the server socket
+        // Bind the server socket (associate the socket with a port on local machine so listen knows which port to listen to)
         bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-        // Listen for connections
+        // Listen (wait) for connections with 5 incoming connections allowed in queue
         listen(serverSocket, 5);
 
         // Accept a client connection
@@ -41,8 +42,9 @@ int main(int argc, char **argv)
         {
             // Receive data from the client
             char buffer[1024] = {0};
+            send(clientSocket, "Enter message: \n", 15, 0);
             recv(clientSocket, buffer, sizeof(buffer), 0);
-            std::cout << "Message from client: " << buffer << std::endl;
+            std::cout << "Message received: " << buffer;
             if (strncmp(buffer, "EXIT", 4) == 0)
                 break ;
         }
@@ -50,7 +52,7 @@ int main(int argc, char **argv)
         // Close the server socket
         close(clientSocket);
         close(serverSocket);
-        std::cout << "Exiting..." << std::endl;
+        std::cout << "Exiting server..." << std::endl;
     }
     else
         std::cout << "Usage: ./ircserv <port> <password>" << std::endl;
