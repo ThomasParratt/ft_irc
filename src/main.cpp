1f20916e410d480d9b1f6c6b22ca3bf9398b1579
root <root@LAPTOP-J70A2AVE>
2024-11-27 14:21:52 +0200

    working

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,46 @@
 #include <unistd.h>
 #include <string.h>
 
-// write in a more c++ way
-// add classes
-// add poll
-// add error management
+std::string    nick(char *buffer)
+{
+    std::cout << "Storing nickname" << std::endl;
+    std::string nickname;
+    std::string message(buffer);
+    //std::cout << "message = " << message << std::endl;
+    size_t startPos = message.find("NICK ");
+    if (startPos != std::string::npos)
+    {
+        size_t endPos = message.find("\n", startPos);
+        if (endPos != std::string::npos)
+        {
+            nickname = message.substr(startPos + 5, endPos - (startPos + 6));
+            //std::cout << "nickname = " << nickname << std::endl;
+        }
+    }
+    return (nickname);
+}
+
+void    authenticate(char *buffer, std::string password, int clientSocket)
+{
+    std::cout << "Checking password" << std::endl;
+    std::string message(buffer);
+    //std::cout << "message = " << message << std::endl;
+    size_t startPos = message.find("PASS ");
+    if (startPos != std::string::npos)
+    {
+        size_t endPos = message.find("\n", startPos);
+        if (endPos != std::string::npos)
+        {
+            std::string clientPassword = message.substr(startPos + 5, endPos - (startPos + 6));
+            //std::cout << "clientPassword = " << clientPassword << std::endl;
+            if (clientPassword != password)
+            {
+                send(clientSocket, "Error: Password incorrect\r\n", 26, 0);
+                exit(1);
+            }
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -15,6 +51,8 @@ int main(int argc, char **argv)
     {
         u_int16_t port = std::stoi(argv[1]);
         std::string password = argv[2];
+        std::string nickname;
+        bool welcomeSent = false;
 
         // Create the server socket
         int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -42,11 +80,32 @@ int main(int argc, char **argv)
         {
             // Receive data from the client
             char buffer[1024] = {0};
-            send(clientSocket, "Enter message: \n", 15, 0);
-            recv(clientSocket, buffer, sizeof(buffer), 0);
-            std::cout << "Message received: " << buffer;
-            if (strncmp(buffer, "EXIT", 4) == 0)
+            ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesRead == -1)
+            {
+                std::cerr << "Error reading from socket" << std::endl;
                 break ;
+            }
+            else if (bytesRead == 0)
+            {
+                // Client closed connection
+                std::cout << "Client disconnected" << std::endl;
+                break ;
+            }
+            std::cout << buffer;
+            authenticate(buffer, password, clientSocket);
+            nickname = nick(buffer);
+            if (!nickname.empty())
+            {
+                std::string response = "You are now known as " + nickname + "\r\n";
+                send(clientSocket, response.c_str(), response.size(), 0);
+                if (!welcomeSent)
+                {
+                    std::string welcomeMessage = ":ircserver 001 " + nickname + " :Welcome to the IRC network " + nickname + "\r\n"; //NEED TO FIX THIS
+                    send(clientSocket, welcomeMessage.c_str(), welcomeMessage.size(), 0);
+                    welcomeSent = true;
+                }
+            }
         }
         
         // Close the server socket
