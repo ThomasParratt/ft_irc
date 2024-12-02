48c3b7439aba2d42b87de51c3fffec368ceabd18
Thomas Parratt <tparratt@c2r2p5.hive.fi>
2024-12-02 12:50:00 +0200

    poll working

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
+#include <vector>
+#include <poll.h>
+
+// write in a more c++ way
+// add classes
+// add error management
+// implement password
+// implement commands
+// handle exits gracefully
+// tidy code
+// smaller functions
 
 std::string messageParam(char *buffer, std::string message)
 {
@@ -20,6 +31,7 @@ std::string messageParam(char *buffer, std::string message)
 
 int    handleMessages(char *buffer, std::string password, std::string nickname, int clientSocket, bool welcomeSent)
 {
+    std::cout << "Handling messages" << std::endl;
     std::string clientPassword = messageParam(buffer, "PASS ");
     if (!clientPassword.empty())
     {
@@ -62,7 +74,7 @@ int main(int argc, char **argv)
         // Create the server socket
         int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-        // Set the SO_REUSEADDR option
+        // Set the SO_REUSEADDR option to allow resuse of the port
         int opt = 1;
         setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
@@ -76,40 +88,91 @@ int main(int argc, char **argv)
         bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
         // Listen (wait) for connections with 5 incoming connections allowed in queue
-        listen(serverSocket, 5);
+        listen(serverSocket, 5); // Is this required?
 
-        // Accept a client connection
-        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        // Set up poll structures
+        std::vector<struct pollfd> pollfds;
+        std::vector<int> clientSockets;
+        pollfds.push_back({serverSocket, POLLIN, 0}); // Add server socket to poll with the event POLLIN (data available to read)
 
         while (true)
         {
-            // Receive data from the client
-            char buffer[1024] = {0};
-            ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-            if (bytesRead == -1)
+            // Continuously monitors sockets for events using poll()
+            int pollCount = poll(pollfds.data(), pollfds.size(), -1);
+            // Check the server socket for new incoming connections
+            if (pollfds[0].revents & POLLIN)
             {
-                std::cerr << "Error reading from socket" << std::endl;
-                break ;
+                // Accept a client 
+                int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+                // Add the new client socket to the poll set
+                pollfds.push_back({clientSocket, POLLIN, 0});
+                clientSockets.push_back(clientSocket);
+
+                std::cout << "Client connected" << std::endl;
+
+                char buffer[1024] = {0};
+                ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+                std::cout << "********* 1 **********" << std::endl;
+                std::cout << buffer;
+                int ret = handleMessages(buffer, password, nickname, clientSocket, welcomeSent);
+                if (ret == 1) 
+                    break ;
+                else if (ret == 2)
+                    welcomeSent = true;
             }
-            else if (bytesRead == 0)
+
+            // Handle client communication by iterating through the client sockets and check for incoming data
+            for (size_t i = 1; i < pollfds.size(); i++)
             {
-                // Client closed connection
-                std::cout << "Client disconnected" << std::endl;
-                break ;
+                if (pollfds[i].revents & POLLIN)
+                {
+                    char buffer[1024] = {0};
+                    ssize_t bytesRead = recv(pollfds[i].fd, buffer, sizeof(buffer), 0);
+
+                    if (bytesRead == -1)
+                    {
+                        std::cerr << "Error reading from socket" << std::endl;
+                        close(pollfds[i].fd);
+                        pollfds.erase(pollfds.begin() + i);
+                        clientSockets.erase(clientSockets.begin() + i);
+                        i--;
+                        continue ;
+                    }
+                    else if (bytesRead == 0)
+                    {
+                        // Client closed connection
+                        std::cout << "Client disconnected" << std::endl;
+                        close(pollfds[i].fd);
+                        pollfds.erase(pollfds.begin() + i);
+                        clientSockets.erase(clientSockets.begin() + i);
+                        i--;
+                        continue ;
+                    }
+                    // Process the message from the client
+                    //std::cout << "Message from client: " << buffer;
+                    std::cout << "********* 2 **********" << std::endl;
+                    std::cout << buffer;
+                    int ret = handleMessages(buffer, password, nickname, clientSockets[i], welcomeSent);
+                    if (ret == 1) 
+                        break ;
+                    else if (ret == 2)
+                        welcomeSent = true;
+
+                    //send(pollfds[i].fd, buffer, sizeof(buffer), 0);
+                }
             }
-            std::cout << buffer;
-            int ret = handleMessages(buffer, password, nickname, clientSocket, welcomeSent);
-            if (ret == 1) 
-                break ;
-            else if (ret == 2)
-                welcomeSent = true;
         }
-        // Close the server socket
-        close(clientSocket);
         close(serverSocket);
+        for (size_t i = 1; i < pollfds.size(); i++)
+        {
+            close(pollfds[i].fd);
+            close(clientSockets[i]);
+        }
         std::cout << "Exiting server..." << std::endl;
     }
     else
         std::cout << "Usage: ./ircserv <port> <password>" << std::endl;
 }
+
 
