707fcdfb55648d0097c6996b85848f93086887d3
Thomas Parratt <tparratt@c3r2p2.hive.fi>
2024-12-17 14:25:16 +0200

    kick working

--- src/kick.cpp
+++ src/kick.cpp
@@ -108,9 +108,6 @@ int		Server::inviteCommand(Msg msg, int clientSocket, Client &client)
 	return (0);
 }
 
-// works for "/kick bob"
-// need extra checks if "/kick #channel bob" // For example, if the kick command is executed while in a different channel
-// from main screen and from different channel screens
 int		Server::kickCommand(Msg msg, int clientSocket, Client &client)
 {
 	std::cout << "KICK COMMAND" << std::endl;
@@ -136,29 +133,29 @@ int		Server::kickCommand(Msg msg, int clientSocket, Client &client)
 									int i = getChannelIndex(msg.parameters[0], channel_names);
 									std::string kick = ":" + kicker.nickname + " KICK " + channel.name + " " + msg.parameters[1] + "\r\n";
 									broadcastToChannel(channel_names[i], kick);
-									printChannels();
 									removeUser(msg.parameters[1], msg.parameters[0], "You have been kicked from");
-									printChannels();
-									//return (0); // do we need these returns?
 								}
 								else
 								{
-									// need to use message 441
+									// here
+									std::cout << "KICKEE DOESN'T EXIST" << std::endl;
 									std::string notice = ":ircserv NOTICE " + channel.name + " :" + msg.parameters[1] + " is not on this channel\r\n";
 									send(clientSocket, notice.c_str(), notice.size(), 0);
-									std::string message_441 = ":ircserv 441 " + client.getNickname() + " " + msg.parameters[0] + " " + msg.parameters[1] + " :They aren't on that channel\r\n";
-									send(clientSocket, message_441.c_str(), message_441.size(), 0);
-									//return (0); // do we need these returns?
+									// std::string priv = ":ircserv PRIVMSG " + channel.name + " :" + msg.parameters[1] + " is not on this channel\r\n";
+									// send(clientSocket, priv.c_str(), priv.size(), 0);
+									// std::string message_441 = ":ircserv 441 " + client.getNickname() + " " + msg.parameters[0] + " " + msg.parameters[1] + " :They aren't on that channel\r\n";
+									// send(clientSocket, message_441.c_str(), message_441.size(), 0); //THIS WILL EXIT THE CHANNEL WINDOW
 								}
 							}
 							else
 							{
 								std::cout << "USER IS NOT AN OPERATOR" << std::endl;
-								std::string notice = ":ircserv NOTICE " + channel.name + " :You're not channel operator\r\n";
+								std::string notice = ":ircserv NOTICE " + client.getNickname() + " :You're not channel operator for " + channel.name + "\r\n";
 								send(clientSocket, notice.c_str(), notice.size(), 0);
-								std::string message_482 = ":ircserv 482 " + client.getNickname() + " " + channel.name + " :You're not channel operator\r\n";
-								send(clientSocket, message_482.c_str(), message_482.size(), 0);
-								//return (0); // do we need these returns?
+								std::string priv = ":ircserv PRIVMSG " + channel.name + " :You're not channel operator\r\n";
+								send(clientSocket, priv.c_str(), priv.size(), 0);
+								// std::string message_482 = ":ircserv 482 " + client.getNickname() + " " + channel.name + " :You're not channel operator\r\n";
+								// send(clientSocket, message_482.c_str(), message_482.size(), 0); //THIS WILL EXIT THE CHANNEL WINDOW
 							}
 						}
 					}
