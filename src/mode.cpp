da436fe098d77bb6dbf12edd3bb6769ff3ca6f4e
quietmid <ninjonglu@gmail.com>
2025-01-09 13:51:29 +0200

    debug comments taken out of /mode besides +l and +k, those are commented out

--- src/mode.cpp
+++ src/mode.cpp
@@ -39,8 +39,6 @@ int		Server::modeCommand(Msg msg, int clientSocket, Client &client)
 		send(clientSocket, errMsg.c_str(), errMsg.size(), 0);
 		return (1);
 	}
-	printMsg(msg); //debug
-	//find the current channel to apply the mode + flag
 	Channel* tarChannel = getChannel(msg.parameters[0]);
 	if (tarChannel == nullptr)
 	{
@@ -48,40 +46,34 @@ int		Server::modeCommand(Msg msg, int clientSocket, Client &client)
 		send(clientSocket, message.c_str(), message.size(), 0);
 		return (1);
 	}
-	//parse the command
-	std::cout << "Channel name: " << tarChannel->name << std::endl; //debug
 	if (msg.parameters[1].empty())
 	{
 		std::string u_modeIs = ":ircserver 221 " + client.getNickname() + " User Modes=i,t,k,o,l" + "\r\n";
     	send(client.getSocket(), u_modeIs.c_str(), u_modeIs.size(), 0);
+		return(1);
 	}
 	else if (msg.parameters[1] == "+i" || msg.parameters[1] == "-i")
 	{
-		std::cout << "i" << std::endl; //debug
 		tarChannel->invite_only = (msg.parameters[1] == "+i") ? true : false;
-		std::cout << "+i/-i" << std::endl; //debug
 		std::string inviteMsg = ":" + client.getNickname() + " MODE " + msg.parameters[0] + " " + msg.parameters[1] + "\r\n";
 		broadcastToChannel(*tarChannel, inviteMsg);
 	}
 	else if (msg.parameters[1] == "+t" || msg.parameters[1] == "-t")
 	{
 		tarChannel->topic_requires_operator = (msg.parameters[1] == "+t") ? true : false;
-		std::cout << "+t/-t" << std::endl; //debug
 		std::string topicMsg = ":" + client.getNickname() + " MODE " + msg.parameters[0] + " " + msg.parameters[1] + "\r\n";
 		broadcastToChannel(*tarChannel, topicMsg);
 	}
 	else if (msg.parameters[1] == "+k" || msg.parameters[1] == "-k")
 	{
-		std::cout << msg.parameters[1] << std::endl; //debug
 		if (msg.parameters[1] == "+k" && msg.parameters[2].empty()) //check if key is empty or not. not sure if this is the right error code to return
 		{
-			std::cout << "Key is empty" << std::endl; //debug
 			std::string errMsg = ":ircserver 525" + client.getNickname() + " " + msg.parameters[0] + " :Key is not well-formed\r\n";
 			send(clientSocket, errMsg.c_str(), errMsg.size(), 0);
 			return (1);
 		}
 		else if (msg.parameters[1] == "+k") {
-			std::cout << "Setting Channel Key: " << msg.parameters[2] << std::endl; //debug
+			// std::cout << "Setting Channel Key: " << msg.parameters[2] << std::endl; //debug
 			tarChannel->channel_key = msg.parameters[2];
 			tarChannel->keyRequired = true;
 			std::string keyMsg = ":" + client.getNickname() + " MODE " + msg.parameters[0] + " +k " + msg.parameters[2] + "\r\n";
@@ -89,7 +81,7 @@ int		Server::modeCommand(Msg msg, int clientSocket, Client &client)
 		}
 		else if (msg.parameters[1] == "-k")
 		{
-			std::cout << "Removing Channel Key" << std::endl; //debug
+			// std::cout << "Removing Channel Key" << std::endl; //debug
 			tarChannel->channel_key = "";
 			tarChannel->keyRequired = false;
 			std::string keyMsg = ":" + client.getNickname() + " MODE " + msg.parameters[0] + " -k *" + "\r\n";
@@ -98,9 +90,6 @@ int		Server::modeCommand(Msg msg, int clientSocket, Client &client)
 	}
 	else if (msg.parameters[1] == "+o" || msg.parameters[1] == "-o")
 	{			
-		std::cout << "o" << std::endl; //debug
-		//TODO: Give/take channel operator privilege
-		// need to check if names is given before
 		if (msg.parameters[2].empty())
 		{
 			std::string errMsg = ":ircserver 461 " + client.getNickname() + " MODE :Not enough parameters\r\n";
@@ -112,12 +101,8 @@ int		Server::modeCommand(Msg msg, int clientSocket, Client &client)
 		{
 			if (tarChannel->channel_users[i].nickname == msg.parameters[2])
 			{
-				// need to make sure it is not the client itself
 				nickExists = true;
-				std::cout << "found: " << tarChannel->channel_users[i].nickname << std::endl; //debug
-				std::cout << tarChannel->channel_users[i].operator_permissions << std::endl; //debug
 				tarChannel->channel_users[i].operator_permissions = (msg.parameters[1] == "+o") ? true : false;
-				std::cout << tarChannel->channel_users[i].operator_permissions << std::endl; //debug
 				std::string chMsg = ":" + client.getNickname() + " MODE " + msg.parameters[0] + " " + msg.parameters[1] + " " + msg.parameters[2] + "\r\n";
 				broadcastToChannel(*tarChannel, chMsg);
 				break;
