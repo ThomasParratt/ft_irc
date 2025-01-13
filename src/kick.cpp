93783c5183567b75af58abd8b729329243aca185
Thomas Parratt <tparratt@c1r5p17.hive.fi>
2025-01-13 14:02:20 +0200

    kick segfault fixed

--- src/kick.cpp
+++ src/kick.cpp
@@ -51,10 +51,13 @@ int		Server::kickCommand(Msg msg, int clientSocket, Client &client)
 									broadcastToChannel(channel_names[i], kick);
 									removeUser(msg.parameters[1], msg.parameters[0], "You have been kicked from", 1);
 									client.leaveChannel(msg.parameters[0]);
-									for (int i = 0; i < sizeof(channel.invited); i++)
+									if (!channel.invited.empty())
 									{
-										if (msg.parameters[1] == channel.invited[i])
-											channel.invited.erase(channel.invited.begin() + i);
+										for (int i = 0; i < sizeof(channel.invited); i++)
+										{
+											if (msg.parameters[1] == channel.invited[i])
+												channel.invited.erase(channel.invited.begin() + i);
+										}
 									}
 								}
 								else
